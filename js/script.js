5aed328d1fc7e66a5f5ce0eb8a9a8e315de842a4
R0st <dotarost@mail.ru>
2022-02-06 13:52:23 +0200

    add 018

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// const num = 'track';
+
+// console.log(num.length);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+
+// console.log(logg.slice(6,11));
+// console.log(logg.substring(6,11));
+// console.log(logg.substr(6,5));
+
+const num = "12.2";
+
+console.log(parseInt(num));
+
